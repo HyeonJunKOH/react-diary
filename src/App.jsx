872c30d012c9fa9e09f967bd62e38ab848d49b73
HyeonJunKOH <hyeonjun9194@gmail.com>
2024-04-12 17:58:57 +0900

    4월12일 감정일기장 프로젝트 시작

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import './App.css';
-import {Routes, Route } from "react-router-dom";
+import { useReducer } from 'react';
+import {Routes, Route} from "react-router-dom";
 import Diary from './pages/Diary';
 import New from './pages/New';
 import Home from './pages/Home';
 import Notfound from './pages/Notfound';
+import Edit from './pages/Edit';
 
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" : 새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+const mockData = [
+  {
+    id : 1,
+    createdData: new Date().getTime(),
+    emotionId : 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id : 2,
+    createdData: new Date().getTime(),
+    emotionId : 2,
+    content: "2번 일기 내용",
+  },
+
+]
+
+function reducer(state, action){
+  return state;
+}
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
   return (
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/new" element={<New/>}/>
-      <Route path="/diary" element={<Diary/>}/>
-      <Route path="/*" element={<Notfound/>}/>
-    </Routes>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/new" element={<New/>}/>
+        <Route path="/diary/:id" element={<Diary/>}/>
+        <Route path="/edit/:id" element={<Edit/>}/>
+        <Route path="/*" element={<Notfound/>}/>
+      </Routes>
+    </>
+  );
 }
 
 export default App
--- src/pages/Diary.jsx
+++ src/pages/Diary.jsx
@@ -1,4 +1,10 @@
+import { useParams } from "react-router-dom";
+
+
 const Diary = () => {
+    const params = useParams();
+    console.log(params);
+
     return <div>Diary</div>
 }
 
