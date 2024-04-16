13f1a269d580ffdcfe3d70cf85f94325ca242e7f
KOHHyeonJun <tttmmm100@naver.com>
2024-04-16 22:34:13 +0900

    4월16일 Home페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -10,17 +10,22 @@ import Edit from './pages/Edit';
 const mockData = [
   {
     id : 1,
-    createdData: new Date().getTime(),
+    createdDate: new Date("2024-04-16").getTime(),
     emotionId : 1,
     content: "1번 일기 내용",
   },
   {
     id : 2,
-    createdData: new Date().getTime(),
+    createdDate: new Date("2024-04-15").getTime(),
     emotionId : 2,
     content: "2번 일기 내용",
   },
-
+  {
+    id: 3,
+    createdDate: new Date("2024-03-18").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  },
 ]
 
 function reducer(state, action){
@@ -42,8 +47,8 @@ function reducer(state, action){
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
