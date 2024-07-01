b454882dcfb85c4f4c98bf7d36ee65491d11b8f6
HyeonJunKOH <hyeonjun9194@gmail.com>
2024-07-01 15:57:17 +0900

    7월1일 버튼 컴포넌트 리팩토링 완료

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,19 @@
 import "./Button.css";
 
-const Button = ({text, type, onClick}) => {
-    return (
-    <button onClick={onClick} className={`Button Button_${type}`}>
-        {text}
-    </button>
-    );
-};
+
+// props 타입 정의
+interface ButtonProps {
+    text: string;
+    type: "primary" | "secondary" | "danger";
+    onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({text,type,onClick}) =>{
+    return(
+        <button onClick={onClick} className={`Button Button_${type}`}>
+            {text}
+        </button>
+    )
+}
 
 export default Button;
